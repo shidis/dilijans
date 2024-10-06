219568d01fc3d466f171f77b2d0ced3958491f54
qwe <qwe>
2024-10-06 22:34:48 +0300

    Test branch init

--- assets/cache/jsmart/d084493ca99b7f1a4bc01571376b809f.js
+++
@@ -1,4 +0,0 @@
-(function($){var F=$.fancybox;F.helpers.buttons={defaults:{skipSingle:false,position:'top',tpl:'<div id="fancybox-buttons"><ul><li><a class="btnToggle" href="javascript:;"></a></li></ul></div>'},list:null,buttons:null,beforeLoad:function(opts,obj){if(opts.skipSingle&&obj.group.length<2){obj.helpers.buttons=false;return;}},afterShow:function(opts,obj){var buttons=this.buttons;this.list=$(opts.tpl).addClass(opts.position).insertBefore('.fancybox-skin');buttons={toggle:this.list.find('.btnToggle').click(F.toggle),}
-this.buttons=buttons;this.onUpdate(opts,obj);},onUpdate:function(opts,obj){var toggle;if(!this.buttons){return;}
-toggle=this.buttons.toggle.removeClass('btnDisabled btnToggleOn');if(obj.canShrink){toggle.addClass('btnToggleOn');}else if(!obj.canExpand){toggle.addClass('btnDisabled');}},beforeClose:function(){if(this.list){this.list.remove();}
-this.list=null;this.buttons=null;}};}(jQuery));
